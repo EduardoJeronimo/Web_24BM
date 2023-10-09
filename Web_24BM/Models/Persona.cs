a764158a970ecd2b5c7236138ce8b114be4ab92f
EduardoJeronimo <133821965+EduardoJeronimo@users.noreply.github.com>
2023-10-09 13:43:01 -0500

    Agregar archivos de proyecto.

---
+++ Web_24BM/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Web_24BM.Models
+{
+    [Table("Personas")]
+    public class Persona
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [StringLength(18)]
+        public string CURP { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+        
+    }
+}
