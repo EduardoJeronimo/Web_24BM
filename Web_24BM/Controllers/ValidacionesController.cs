0de5bf0317f5283ba7c3805ca5355f27e7e88451
EduardoJeronimo <133821965+EduardoJeronimo@users.noreply.github.com>
2023-10-16 13:55:08 -0500

    Clase del viernes(13) Tarea Formulario Clase del lunes(16)

---
+++ Web_24BM/Controllers/ValidacionesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_24BM.Models;
+
+namespace Web_24BM.Controllers
+{
+    public class ValidacionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnviarFormulario(Curriculum model)
+        {
+            string mensaje = " ";
+            if (ModelState.IsValid)
+            {
+                mensaje = "Datos Validos";
+                TempData["msj"] = mensaje;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                mensaje = "Datos Incorrectos";
+                return View("Index",model) ;
+            }
+            
+        }
+    }
+}
+
--- Web_24BM/Program.cs
+++ Web_24BM/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Web_24BM.Data;
+using Web_24BM.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+builder.Services.AddTransient<IEmailSenderService, EmailSenderService>();
 
 var app = builder.Build();
 
@@ -30,6 +33,7 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSession();
 
 app.UseRouting();
 
